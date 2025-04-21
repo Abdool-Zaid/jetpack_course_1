73e0e7492d02ff6e780d250e64f17e2d1db28e94
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-21 13:02:34 +0200

    column

--- app/src/main/java/com/example/jetpack_course_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_course_1/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jetpack_course_1.ui.theme.Jetpack_course_1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,17 +23,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             Jetpack_course_1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+                        Print_string(
+                            "string_1",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        Print_string(
+                                "string_2"
+                        )
+                    }
+
                 }
             }
         }
     }
 }
 
-@Composable
+@Composable // composible is a kotlin function
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
@@ -44,4 +53,14 @@ fun GreetingPreview() {
     Jetpack_course_1Theme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun Print_string(str: String , modifier: Modifier = Modifier){
+    Text(
+        text= str,
+        modifier= modifier,
+        color = Color.Green,
+        fontSize = 20.sp
+    )
 }
