e2db8aa887103eba12bd792d1e6bbaccb56f075f
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-21 13:18:15 +0200

    loop

--- app/src/main/java/com/example/jetpack_course_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_course_1/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,16 +25,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             Jetpack_course_1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column {
-                        Print_string(
-                            "string_1",
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                        Print_string(
-                                "string_2"
-                        )
-                    }
+                    Column(
 
+                        modifier = Modifier.padding(innerPadding)
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+
+                    )
+                    {
+                        for (i in 1..9) {
+                            Print_string(
+                                "string_$i"
+
+                            )
+                        }
+                    }
                 }
             }
         }
